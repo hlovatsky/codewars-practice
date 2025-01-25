aca8590ebccc0945aa88a03d6d8ef3c70fdb43cd
Oleksii Hlovatskyi <72668984+hlovatsky@users.noreply.github.com>
2025-01-25 19:33:10 +0200

    fixed Emirps logic

--- src/main/java/five/Emirps.java
+++ src/main/java/five/Emirps.java
@@ -8,7 +8,6 @@ That idea is related with the purpose of this kata: we should select all the pri
 For example: 13, 17 are prime numbers and the reversed respectively are 31, 71 which are also primes,
 so 13 and 17 are "emirps". But primes 757, 787, 797 are palindromic primes,
 meaning that the reversed number is the same as the original, so they are not considered as "emirps" and should be discarded.
-The emirps sequence is registered in OEIS as A006567
 
 Your task:
 Create a function that receives one argument n, as an upper limit, and the return the following array:
@@ -18,16 +17,40 @@ Create a function that receives one argument n, as an upper limit, and the retur
 public class Emirps {
 
     public long[] findEmirp(long n) {
-        long[] emirpNumbers = {13, 17, 31, 37, 71, 73, 79, 97, 107, 113, 149, 157, 167, 179, 199, 311, 337, 347, 359, 389, 701, 709, 733, 739, 743, 751, 761, 769, 907, 937, 941, 953, 967, 971, 983, 991, 1009, 1021, 1031, 1033, 1061, 1069, 1091, 1097, 1103, 1109, 1151, 1153, 1181, 1193, 1201};
-        List<Long> resultList = new ArrayList<>();
-        for (long emirpNumber : emirpNumbers) {
-            if (n >= emirpNumber) {
-                resultList.add(emirpNumber);
+        ArrayList<Integer> emirps = new ArrayList<>();
+        for (int i = 2; i < n; i++) {
+            if (isEmirp(i)) {
+                emirps.add(i);
             }
         }
-        long emirpsSize = resultList.size();
-        long maxEmirp = resultList.stream().max(Long::compareTo).orElse(0L);
-        long emirpSum = resultList.stream().reduce(0L, Long::sum);
-        return new long[]{emirpsSize, maxEmirp, emirpSum};
+
+        int numEmirps = emirps.size();
+        int largestEmirp = emirps.isEmpty() ? 0 : Collections.max(emirps);
+        int sumEmirps = emirps.stream().mapToInt(Integer::intValue).sum();
+
+        return new long[]{numEmirps, largestEmirp, sumEmirps};
+    }
+
+    private boolean isEmirp(int number) {
+        long reversed = reverseNumber(number);
+        return number != reversed && isPrime(number) && isPrime(reversed);
+    }
+
+    private long reverseNumber(long number) {
+        String numberStr = Long.toString(number);
+        String reversedStr = new StringBuilder(numberStr).reverse().toString();
+        return Long.parseLong(reversedStr);
+    }
+
+    private boolean isPrime(long number) {
+        if (number <= 1) {
+            return false;
+        }
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
