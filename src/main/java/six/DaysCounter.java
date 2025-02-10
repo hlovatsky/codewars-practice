dea2329c894ee5ae47a1b1a53c4c2043fa584e55
Oleksii Hlovatskyi <72668984+hlovatsky@users.noreply.github.com>
2025-02-10 17:04:56 +0200

    minor

--- src/main/java/six/DaysCounter.java
+++ src/main/java/six/DaysCounter.java
@@ -7,6 +7,7 @@ import static java.time.temporal.ChronoUnit.DAYS;
 /*
 https://www.codewars.com/kata/59569ba543e2a8ebda00012a
  */
+
 public class DaysCounter {
 
     public long getDaysBetween2Dates(String from, String to) {
